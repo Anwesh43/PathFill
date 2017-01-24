5b2ed11d99b5fb371deff35a90df9f39262ae1fd
Anwesh43 <anweshthecool0@gmail.com>
2017-01-25 02:40:43 +0530

    Testing down for triangle and up for circle

--- app/src/main/java/com/anwesome/ui/pathfilldemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/pathfilldemo/MainActivity.java
@@ -21,9 +21,30 @@ public class MainActivity extends AppCompatActivity {
         pathFillView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this,"Circle Click",Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this,"Circle Down",Toast.LENGTH_SHORT).show();
             }
         });
+
         addContentView(pathFillView,new ViewGroup.LayoutParams(300,300));
+        PathFillView pathFillView1 = new PathFillView(this, PathFillDirection.DOWN, PathFillShape.TRIANGLE, Color.parseColor("#F4511E"));
+        pathFillView1.setX(300);
+        pathFillView1.setY(0);
+        pathFillView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"Triangle Down",Toast.LENGTH_SHORT).show();
+            }
+        });
+        addContentView(pathFillView1,new ViewGroup.LayoutParams(300,300));
+        PathFillView pathFillView2 = new PathFillView(this, PathFillDirection.UP, PathFillShape.CIRCLE, Color.parseColor("#F4511E"));
+        pathFillView2.setX(600);
+        pathFillView1.setY(0);
+        pathFillView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"Circle Up",Toast.LENGTH_SHORT).show();
+            }
+        });
+        addContentView(pathFillView2,new ViewGroup.LayoutParams(300,300));
     }
 }
