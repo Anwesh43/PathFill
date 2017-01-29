90187f03a6efae16298cc8a786c24fd505095c89
Anwesh43 <anweshthecool0@gmail.com>
2017-01-29 14:07:58 +0530

    Using PathFillView in app

--- app/src/main/java/com/anwesome/ui/pathfilldemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/pathfilldemo/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.anwesome.game.pathfill.PathFillDirection;
+import com.anwesome.game.pathfill.PathFillHelper;
 import com.anwesome.game.pathfill.PathFillShape;
 import com.anwesome.game.pathfill.PathFillView;
 
@@ -17,34 +18,52 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        PathFillView pathFillView = new PathFillView(this, PathFillDirection.DOWN, PathFillShape.CIRCLE, Color.parseColor("#F4511E"));
-        pathFillView.setOnClickListener(new View.OnClickListener() {
+        PathFillHelper.addPathFillView(this, PathFillDirection.DOWN, PathFillShape.CIRCLE, Color.parseColor("#F4511E"), new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this,"Circle Down",Toast.LENGTH_SHORT).show();
             }
-        });
-
-        addContentView(pathFillView,new ViewGroup.LayoutParams(300,300));
-        PathFillView pathFillView1 = new PathFillView(this, PathFillDirection.DOWN, PathFillShape.TRIANGLE, Color.parseColor("#F4511E"));
-        pathFillView1.setX(300);
-        pathFillView1.setY(0);
-        pathFillView1.setOnClickListener(new View.OnClickListener() {
+        },300,0);
+        PathFillHelper.addPathFillView(this, PathFillDirection.DOWN, PathFillShape.TRIANGLE, Color.parseColor("#F4511E"), new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this,"Triangle Down",Toast.LENGTH_SHORT).show();
             }
-        });
-        addContentView(pathFillView1,new ViewGroup.LayoutParams(300,300));
-        PathFillView pathFillView2 = new PathFillView(this, PathFillDirection.UP, PathFillShape.CIRCLE, Color.parseColor("#F4511E"));
-        pathFillView2.setX(600);
-        pathFillView1.setY(0);
-        pathFillView2.setOnClickListener(new View.OnClickListener() {
+        },300,300);
+        PathFillHelper.addPathFillView(this, PathFillDirection.UP, PathFillShape.CIRCLE, Color.parseColor("#F4511E"), new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this,"Circle Up",Toast.LENGTH_SHORT).show();
             }
-        });
-        addContentView(pathFillView2,new ViewGroup.LayoutParams(300,300));
+        },300,600);
+//        PathFillView pathFillView = new PathFillView(this, PathFillDirection.DOWN, PathFillShape.CIRCLE, Color.parseColor("#F4511E"));
+//        pathFillView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(MainActivity.this,"Circle Down",Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//
+//        addContentView(pathFillView,new ViewGroup.LayoutParams(300,300));
+//        PathFillView pathFillView1 = new PathFillView(this, PathFillDirection.DOWN, PathFillShape.TRIANGLE, Color.parseColor("#F4511E"));
+//        pathFillView1.setX(300);
+//        pathFillView1.setY(0);
+//        pathFillView1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(MainActivity.this,"Triangle Down",Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//        addContentView(pathFillView1,new ViewGroup.LayoutParams(300,300));
+//        PathFillView pathFillView2 = new PathFillView(this, PathFillDirection.UP, PathFillShape.CIRCLE, Color.parseColor("#F4511E"));
+//        pathFillView2.setX(600);
+//        pathFillView1.setY(0);
+//        pathFillView2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(MainActivity.this,"Circle Up",Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//        addContentView(pathFillView2,new ViewGroup.LayoutParams(300,300));
     }
 }
